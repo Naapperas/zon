61902532625d9e50600f855b9a1007781692df91
Nuno Pereira <nunoafonso2002@gmail.com>
2023-11-23 15:58:57 +0000

    Removed main.py

---
+++ tests/str_test.py
@@ -0,0 +1,28 @@
+import pod
+
+import pytest
+
+
+@pytest.fixture
+def validator():
+    return pod.string()
+
+
+def test_str(validator):
+    assert validator.validate("1")
+
+
+def test_not_float(validator):
+    assert not validator.validate(1.5)
+
+
+def test_not_int(validator):
+    assert not validator.validate(1)
+
+
+def test_not_list(validator):
+    assert not validator.validate([1])
+
+
+def test_not_record(validator):
+    assert not validator.validate({"a": 1})
